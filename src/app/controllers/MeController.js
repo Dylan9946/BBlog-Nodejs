91613d6ff17e838a40374b7fc2e680adef687bb3
kaaa <18521224@gm.uit.edu.vn>
2021-08-04 21:53:41 +0700

    count deleted posts

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -10,15 +10,46 @@ class meController {
 
     // trang chủ của Site
     // phương thức [GET] me/manage/blogs
+    
     manageBlogs(req, res,next){
         // promise
-          BlogPost.find({})
-          .then(blogpost =>  res.render('me/manage-blogs',{
-                blogpost: multiMogooseToObject(blogpost) 
-             })
-          )
-          .catch(err => next())
+        //  vì là bất đồng bộ nên mange blog k thể nhận deletedCount trc khi nó trả về nên dùng promise.all để hiển thị thùng rác đang chứa bao nhiêu bài post đã xóa
+        // 2 tham số đầu vào là blogpost dc lấy ra từ db và số các post đã bị xóa
+        // 
+
+        Promise.all([ BlogPost.find({}), BlogPost.countDocumentsDeleted()] ) // 2 tham số đầu vào 
+         // nhận lại 2 giá trị trả về của 2 tham số truyền vào trên
+        .then(([blogpost,deletedCount]) =>   
+               res.render('me/manage-blogs',{
+                  blogpost: multiMogooseToObject(blogpost), // ném sang view
+                  deletedCount: deletedCount // ném sang view
+
+               })
+        
+        )
+        .catch(()=>{});
+      
+      //   BlogPost.countDocumentsDeleted() 
+      //    .then((deletedCount) => {
+      //       console.log(deletedCount);
+      //    })
+      //    .catch(()=>{});
+
+
+
+      //     BlogPost.find({})
+      //     .then(blogpost =>  res.render('me/manage-blogs',{
+      //           blogpost: multiMogooseToObject(blogpost) 
+      //        })
+      //     )
+      //     .catch(err => next())
+          
     }
+
+
+
+
+
 
    // [GET] me/trash/blogs
     trashBlogs(eq, res,next){
